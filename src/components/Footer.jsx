8a65e82a18a488ba0dbc167433c7e6c75d7c1047
amSebas <sebaslgr@gmail.com>
2023-08-08 16:51:30 -0500

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,18 @@ export default function Footer () {
 
     return(
         <div className="Footer">
-            <div className="footer-up">
+            <div className="footer__1">
                 <div className="footer-p">
                     <p>AVISO IMPORTANTE: Todo el contenido disponible en este sitio web,
                         tiene como único objetivo proporcionar información general 
                         procedente de la API consumida (Coingecko)</p>
                 </div>
-                <div className="footer-c">
+                <div className="footer__2">
                     <p>Crypto Stadistics</p>
                     <p>© 2022</p>
                 </div>
             </div>
-            <div className="footer-down">
+            <div className="footer__3">
                 <p>Developed by</p>
             </div>
         </div>
