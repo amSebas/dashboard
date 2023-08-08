05e2fb2e595056a3e04b156c115c6465b6487708
amSebas <sebaslgr@gmail.com>
2023-08-08 16:48:56 -0500

    si

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,24 @@ import './Footer.css'
 export default function Footer () { 
 
     return(
-        <footer>
-            !footer
-        </footer>
+        <div className="Footer">
+            <div className="footer-up">
+                <div className="footer-p">
+                    <p>AVISO IMPORTANTE: Todo el contenido disponible en este sitio web,
+                        tiene como único objetivo proporcionar información general 
+                        procedente de la API consumida (Coingecko)</p>
+                </div>
+                <div className="footer-c">
+                    <p>Crypto Stadistics</p>
+                    <p>© 2022</p>
+                </div>
+            </div>
+            <div className="footer-down">
+                <p>Developed by</p>
+                <div>
+                    <img src={LogoKarol} alt="logo Karol" />
+                </div>
+            </div>
+        </div>
     )
 }
